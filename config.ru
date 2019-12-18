470fac191318d569886b174b7e09f8e5fa4a1b56
Tung Nguyen <tongueroo@gmail.com>
2019-12-18 00:46:39 +0000

    add example of post and json data

---
+++ config.ru
@@ -0,0 +1,7 @@
+# require 'rubygems'
+# require 'bundler'
+
+# Bundler.require
+
+require File.join(File.dirname(__FILE__), 'hi.rb')
+run Sinatra::Application
--- hi.rb
+++ hi.rb
@@ -1,7 +1,16 @@
 require 'sinatra'
+require 'json'
 
 set :bind, '0.0.0.0'
+set :port, 8888
 
 get '/' do
+  "42\n"
+end
+
+post '/' do
+  # data = JSON.parse(request.body.read.to_s)
+  # puts "data:"
+  # pp data
   "42\n"
 end
