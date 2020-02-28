7e1eb9314f481563e147d3528f2f8721c5233086
Tung Nguyen <tongueroo@gmail.com>
2020-02-28 02:24:16 +0000

    test tar

--- .cody/scripts/ssm.sh
+++ .cody/scripts/ssm.sh
@@ -8,6 +8,10 @@ S3_PATH=$3
 gem install sonic-screwdriver --no-document
 rbenv rehash
 
-env | sort
+cd ..
+tar -czf code.tar -C $CODEBUILD_SRC_DIR .
+cd -
+
+ls
 
 sonic execute --tags Name=backend-admin-box /opt/scripts/solo-ci.sh $ECS_SERVICE $DOCKER_CONTAINER $S3_PATH
