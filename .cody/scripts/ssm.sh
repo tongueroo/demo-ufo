46b87c35dced2a71b5b7ef0f400589dd8f0f0205
Tung Nguyen <tongueroo@gmail.com>
2020-02-28 01:37:27 +0000

    test /opt/scripts/solo-ci.sh

--- .cody/scripts/ssm.sh
+++ .cody/scripts/ssm.sh
@@ -2,4 +2,6 @@
 
 gem install sonic-screwdriver --no-document
 rbenv rehash
-sonic execute --tags Name=backend-admin file://.cody/scripts/solo.sh
+# sonic execute --tags Name=backend-admin file://.cody/scripts/solo.sh
+sonic execute --tags Name=backend-admin /opt/scripts/solo-ci.sh
+
